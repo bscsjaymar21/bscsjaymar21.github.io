7d411766d9341e33b0bfc213425467e41cf37e91
bscsjaymar21 <119992121+bscsjaymar21@users.noreply.github.com>
2022-12-10 14:29:56 +0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,6 +17,18 @@ const About = () => {
         Hi! My name is Jaymar N. Calising from Marihatag Surigao Del Sur, studied at North Eastern Mindanao State University in Lianga Campus. I studied
           Bachelor Sicence in Computer Science 4rth year college in this school year 2022-2023.  This is my project in Human Interaction which we make a website portfolio using reactjs and nodejs.
         </p>
+    <div className="pb-8">
+          <p className="text-2xl font-bold inline border-b-4 border-gray-500">
+            My Hobby
+          </p>
+     <p className="text-xl mt-10">
+       ul class="list-group">
+  <li class="list-group-item">Dancing</li>
+  <li class="list-group-item">Editing</li>
+  <li class="list-group-item">Coding</li>
+</ul>
+        </p>
+        </div>
       </div>
     </div>
   );
